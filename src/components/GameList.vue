b0fdea53f53c0d1a2818a79fa9e2a196f3e037a1
M3ANMAXINE <CHERRERAJ@HOTMAIL.COM>
2020-08-19 18:45:15 -0400

    parte uno terminada

---
+++ src/components/GameList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul >
+      <li v-for="(game,index) in games" :key="index" :style="{'background-color' :game.color}"> {{game.id}} - {{ game.name}} - {{ game.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    games: {
+      type: Array,
+      required: true
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
-    <h1>SEARCH</h1>
+
+    <div>
+      <p> Juegos Disponibles  {{games.length }}</p>
+    </div>
+    <input type="text" placeholder="Ingrese codigo" v-model="search" class="pt-2 pb-3 pl-5" >
+
+    <GameList :games='filterGames' />
   </div>
 </template>
 
 <script>
+import {mapState, mapGetters} from 'vuex'
+import GameList from './GameList'
+
 export default {
+  data() {
+    return {
+      search: null
+      }
+  },
+  components: {
+    GameList
+  },
+  computed: {
+    ...mapState(['games']),
+    ...mapGetters(['findGame']),
+    
+    filterGames () {
+      if (this.search){
+        return this.findGame(this.search)
+        } else{
+          return this.games
+        }
+    }
+  }
 
 }
 </script>
