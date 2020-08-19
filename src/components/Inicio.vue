e7c5828a5ad7f35ea39ea2a380d12b57b141416a
M3ANMAXINE <CHERRERAJ@HOTMAIL.COM>
2020-08-18 21:33:13 -0400

    listo el inicio

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-        <h1>32 Bits</h1>
-        <h2>Juegos de PC y consolas</h2>
+        <h1 v-text="title"></h1>
+        <h2 v-text="subtitle"></h2>
     </div>
 </template>
 <script>
 
 import {mapState} from 'vuex'
 export default {
+  computed: {
+    ...mapState (['title', 'subtitle'])
+  },    
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,10 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {},
+  state: {
+    title: "32 Bits",
+    subtitle: "Juegos de Pc y Consolas"
+  },
   getters: {},
   mutations: {},
   actions: {}
